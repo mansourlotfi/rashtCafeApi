01cc46f730724ede1a393810c3b0f7f5bf8ad161
mansourlotfi <mansourlotfi@gmail.com>
2020-01-04 15:37:59 +0330

    adding mangoose and connection string of mangodb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const placesRoutes = require('./routes/places-route');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
@@ -22,4 +22,12 @@ app.use((error, req, res, next) => {
 	res.status(error.code || 500);
 	res.json({ message: error.message || 'ارور نامشخص' });
 });
-app.listen(5000);
+
+mongoose
+	.connect('mongodb+srv://mansour:fidelio123@cluster0-bgsnp.mongodb.net/places?retryWrites=true&w=majority')
+	.then(() => {
+		app.listen(5000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
