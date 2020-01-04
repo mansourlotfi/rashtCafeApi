510818c71e641e8f671fc8cffbf546c39bcbb605
mansourlotfi <mansourlotfi@gmail.com>
2020-01-04 12:23:36 +0330

    backend basic fase finished

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v4');
+const { validationResult } = require('express-validator');
 const HttpError = require('../models/http-error');
 const DUMMY_USERS = [
 	{
@@ -12,6 +13,10 @@ const getUsers = (req, res, next) => {
 	res.json({ users: DUMMY_USERS });
 };
 const signup = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		throw new HttpError('اطلاعات وارد شده مناسب نمی باشد', 422);
+	}
 	const { name, email, password } = req.body;
 
 	const hasUser = DUMMY_USERS.find((u) => u.email === email);
